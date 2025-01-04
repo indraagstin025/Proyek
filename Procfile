04e0c9542f161ef19a15de1e2a420048bcf46eb3
indraagstin025 <indraagustin012@gmail.com>
2025-01-04 19:03:56 +0700

    push 2

--- Procfile
+++ Procfile
@@ -1,1 +1,9 @@
-web: gunicorn run:app
+nginx:
+  root: public_html/app/static
+  passenger:
+    enabled: "on"
+    app_env: development
+    app_root: public_html
+    python: .pyenv/shims/python
+    app_type: python
+    startup_file: wsgi.py
--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,12 @@
-# wsgi.py
-
 from app import create_app
 
-application = create_app()
+# Buat aplikasi menggunakan konfigurasi production
+application = create_app(config_name='production')
 
-if __name__ == "__main__":
-    application.run()
+# Pastikan .env telah dimuat sebelumnya
+import os
+from dotenv import load_dotenv
+
+env_file = os.path.join(os.getcwd(), '.env')
+if os.path.exists(env_file):
+    load_dotenv(env_file)
