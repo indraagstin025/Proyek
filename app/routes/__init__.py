9e0b14ca53ded4a6208f53f66527b12f5f3031d4
indraagstin025 <indraagustin012@gmail.com>
2025-01-04 19:39:23 +0700

    push

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,7 +1,7 @@
 # app/routes/__init__.py
 from app.routes.auth import blueprint as auth_bp
 from app.routes.document import blueprint as document_bp
-from app.routes.dashboard import Blueprint as dashboard_bp
+from app.routes.dashboard import blueprint as dashboard_bp
 
 def register_blueprints(app):
     app.register_blueprint(auth_bp, url_prefix='/auth')
