39e9816befb8a958c95ca83e5cc67e8547665852
indraagstin025 <indraagustin012@gmail.com>
2025-01-04 20:42:39 +0700

    pushhhhhhhhh

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,12 @@
 import os
-from app import create_app
+from app import create_app  # Impor fungsi create_app dari app/__init__.py
 
 # Gunakan variabel lingkungan untuk menentukan environment
 config_name = os.getenv('FLASK_ENV', 'production')
+
+# Buat instance aplikasi Flask menggunakan fungsi create_app
 application = create_app(config_name=config_name)
 
 if __name__ == "__main__":
-    # Jalankan aplikasi Flask
-    application.run()
+    # Jalankan aplikasi Flask hanya jika dijalankan langsung
+    application.run(debug=(config_name == 'development'))
